75af8d4f2841e4681fb947905dea19af3b35df54
itsasamiracle <samirgandhi777@gmail.com>
2025-08-19 08:44:42 -0500

    Create Position.java

---
+++ Position.java
@@ -0,0 +1,18 @@
+public class Position {
+    int row;
+    int col;
+    public Position(int r, int c)
+    {
+        row = r;
+        col = c;
+
+    }
+    public int getRow()
+    {
+        return row;
+    }
+    public int getCol()
+    {
+        return col;
+    }
+}
